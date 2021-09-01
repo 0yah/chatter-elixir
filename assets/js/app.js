8799690f4685f420b623d19ed95242242c5d35de
0yaah <42600121+0yah@users.noreply.github.com>
2021-09-01 09:54:12 +0300

    Socket initialized succesfully

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,92 @@ import "../css/app.css"
 //
 // Import deps with the dep name or local files with a relative path, for example:
 //
-//     import {Socket} from "phoenix"
+import { Socket, Presence } from "phoenix"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+
+//gets the user from the conn.params["user"] which is referenced using the ID tag with value of "user"
+let user = document.getElementById("user").innerText;
+
+let socket = new Socket("/socket", {
+    params: { user }
+});
+
+socket.connect();
+
+
+let presences = {}
+
+let formattedTimestamp = (Ts) => {
+
+    let date = new Date(Ts);
+    return date.toLocaleString();
+}
+
+let listBy = (user, { metas }) => {
+
+    console.log(metas);
+    return {
+
+        user,
+        onlineAt: formattedTimestamp(metas[0].online_at)
+    }
+};
+
+
+//get the user list element
+let userList = document.getElementById("userList");
+
+let render = presences => {
+
+    return userList.innerHTML = Presence.list(presences, listBy).map(presence => `
+    
+    <li>
+    ${presence.user}
+    <br/>
+    <small>online since ${presence.onlineAt}</small>
+    </li>
+
+    `).join("");
+
+}
+
+//Channel defined in channels/room_channel.ex
+let room = socket.channel("room:lobby");
+/*
+
+When the server send us the state of everyone who is online which happens when we first connect or disconnect
+The event will send everything to the html
+
+*/
+room.on("presence_state", state =>{
+
+    presences = Presence.syncState(presences, state);
+    render(presences);
+
+});
+
+
+room.on("presence_diff", diff =>{
+
+    presences = Presence.syncDiff(presences, diff);
+    render(presences);
+
+});
+
+room.join();
+
+
+let messageInput = document.getElementById("newMessage")
+messageInput.addEventListener("keypress", (e)=>{
+
+    /*
+    If enter is pressed and the input is not blank
+    */
+    if(e.keyCode == 13 && e.target.value != ""){
+        room.push("message:new", e.target.value);
+        messageInput.value = "";
+    }
+});
---
+++ lib/chatter_web/channels/room_channel.ex
@@ -0,0 +1,61 @@
+defmodule ChatterWeb.RoomChannel do
+    use ChatterWeb, :channel
+    alias ChatterWeb.Presence
+    
+
+    """
+
+    self() - user
+    :after_join atom is corresponded with the callback function handle_info
+
+    """
+    def join("room:lobby", _, socket) do
+        send self(), :after_join
+        {:ok, socket}
+    end
+
+
+    # Callback function
+    def handle_info(:after_join, socket) do
+        """
+
+        Track at the socket 
+        Assign the user then track when the user logged into the socket
+
+        """
+        Presence.track(socket, socket.assigns.user, %{
+            online_at: System.os_time(:millisecond)
+        })
+
+        """
+        
+        Send it back to the socket and update the list
+
+        """
+        push socket, "presence_state", Presence.list(socket)
+        {:noreply, socket}
+    end
+
+
+    """
+    
+    Event handler for message:new
+
+    Broadcasts the messsage to everyone
+
+    """
+    def handle_in("message:new", message, socket) do
+
+        IO.puts("message")
+
+        broadcast! socket, "message:new", %{
+            user: socket.assigns.user,
+            body: message,
+            timestamp: System.os_time(:millisecond)
+        }
+
+        {:noreply, socket}
+    end
+
+
+end
